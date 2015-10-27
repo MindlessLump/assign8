952a84c371100b9cdf2b37c8ad55b1a4e0f8abbb
Erik Martin <erik.martin.mc@gmail.com>
2015-10-26 21:52:25 -0600

    Adjusted file paths and indentation of GraphUtil

--- GraphUtil.java
+++ GraphUtil.java
@@ -31,7 +31,7 @@ public class GraphUtil {
 		g.addEdge("V5", "V3");
 		g.addEdge("V6", "V7");
 
-		g.generateDotFile("graph.dot");
+		g.generateDotFile("src/assign8/graph.dot");
 
 		// build another sample graph
 		g = new Graph();
@@ -49,22 +49,22 @@ public class GraphUtil {
 		g.addEdge("7", "1");
 		g.addEdge("7", "6");
 
-		g.generateDotFile("graph2.dot");
+		g.generateDotFile("src/assign8/graph2.dot");
 			
-		System.out.println(GraphUtil.breadthFirstSearch("examplegraph8.dot", "San Diego", "Atlanta"));
+		System.out.println(GraphUtil.breadthFirstSearch("src/assign8/examplegraph8.dot", "San Diego", "Atlanta"));
 
 	}
 	
 
-  /**
-   * Performs a topological sort of the vertices in a directed acyclic graph. (See Lecture 14 for the algorithm.)
-   * 
-   * Throws an UnsupportedOperationException if the graph is undirected or cyclic.
-   * 
-   * @param filename
-   *          -- Name of a file in DOT format, which specifies the graph to be sorted.
-   * @return a list of the vertex names in sorted order
-   */
+	/**
+	 * Performs a topological sort of the vertices in a directed acyclic graph. (See Lecture 14 for the algorithm.)
+	 * 
+	 * Throws an UnsupportedOperationException if the graph is undirected or cyclic.
+	 * 
+	 * @param filename
+	 *          -- Name of a file in DOT format, which specifies the graph to be sorted.
+	 * @return a list of the vertex names in sorted order
+	 */
 	public static List<String> topologicalSort(String filename) {
 		//Generate a graph from the file
 		Graph g = buildGraphFromDot(filename);
@@ -108,23 +108,23 @@ public class GraphUtil {
 			output.add(v.getName());
 		}
 		return output;
-    }
+	}
 
-  /**
-   * Performs a breadth-first search of a graph to determine the shortest path from a starting vertex to an ending vertex.
-   * (See Lecture 14 for the algorithm.)
-   * 
-   * Throws an UnsupportedOperationException if the graph is undirected or if the starting or ending vertex does not exist in the graph.
-   * 
-   * @param filename
-   *          -- Name of a file in DOT format, which specifies the graph to be sorted.
-   * @param start
-   *          -- Name of the starting vertex in the path.
-   * @param end
-   *          -- Name of the ending vertex in the path.
-   * @return a list of the vertices that make up the shortest path from the starting vertex (inclusive) to the ending vertex (inclusive).
-   */
-  	public static List<String> breadthFirstSearch(String filename, String start, String end) {
+	/**
+	 * Performs a breadth-first search of a graph to determine the shortest path from a starting vertex to an ending vertex.
+	 * (See Lecture 14 for the algorithm.)
+	 * 
+	 * Throws an UnsupportedOperationException if the graph is undirected or if the starting or ending vertex does not exist in the graph.
+	 * 
+	 * @param filename
+	 *          -- Name of a file in DOT format, which specifies the graph to be sorted.
+	 * @param start
+	 *          -- Name of the starting vertex in the path.
+	 * @param end
+	 *          -- Name of the ending vertex in the path.
+	 * @return a list of the vertices that make up the shortest path from the starting vertex (inclusive) to the ending vertex (inclusive).
+	 */
+	public static List<String> breadthFirstSearch(String filename, String start, String end) {
   		//Generate a graph from the file
   		Graph g = buildGraphFromDot(filename);
   		Map<String, Vertex> vertices = g.vertices();
@@ -180,84 +180,83 @@ public class GraphUtil {
 		return new ArrayList<String>();
   	}
 
-  /**
-   * Builds a graph according to the edges specified in the given DOT file (e.g., "a -- b" or "a -> b").
-   * Accepts directed ("digraph") or undirected ("graph") graphs.
-   * 
-   * Accepts many valid DOT files (see examples posted with assignment).
-   * --accepts \\-style comments 
-   * --accepts one edge per line or edges terminated with ; 
-   * --does not accept attributes in [] (e.g., [label = "a label"])
-   * 
-   * @param filename
-   *          -- name of the DOT file
-   */
-  private static Graph buildGraphFromDot(String filename) {
-    // creates a new, empty graph (CHANGE AS NEEDED)
-    Graph g = new Graph();
+	/**
+	 * Builds a graph according to the edges specified in the given DOT file (e.g., "a -- b" or "a -> b").
+	 * Accepts directed ("digraph") or undirected ("graph") graphs.
+	 * 
+	 * Accepts many valid DOT files (see examples posted with assignment).
+	 * --accepts \\-style comments 
+	 * --accepts one edge per line or edges terminated with ; 
+	 * --does not accept attributes in [] (e.g., [label = "a label"])
+	 * 
+	 * @param filename
+	 *          -- name of the DOT file
+	 */
+	@SuppressWarnings("resource")
+	private static Graph buildGraphFromDot(String filename) {
+		// creates a new, empty graph (CHANGE AS NEEDED)
+	    Graph g = new Graph();
+	
+	    Scanner s = null;
+	    try {
+	    	s = new Scanner(new File(filename)).useDelimiter(";|\n");
+	    } catch (FileNotFoundException e) {
+	    	System.out.println(e.getMessage());
+	    }
+	
+	    // Determine if graph is directed or not (i.e., look for "digraph id {" or
+	    // "graph id {")
+	    String line = "", edgeOp = "";
+	    while (s.hasNext()) {
+	    	line = s.next();
+	    	
+	    	// Skip //-style comments.
+	    	line = line.replaceFirst("//.*", "");
+	
+	    	if (line.indexOf("digraph") >= 0) {
+	    		g.setDirected(true); // Denotes that graph is directed (CHANGE AS NEEDED)
+	    		edgeOp = "->";
+	    		line = line.replaceFirst(".*\\{", "");
+	    		break;
+	    	}
+	    	if (line.indexOf("graph") >= 0) {
+	    		g.setDirected(false); // Denotes that graph is undirected (CHANGE AS NEEDED)
+	    		edgeOp = "--";
+	    		line = line.replaceFirst(".*\\{", "");
+	    		break;
+	    	}
+	    }
 
-    Scanner s = null;
-    try {
-      s = new Scanner(new File(filename)).useDelimiter(";|\n");
-    } catch (FileNotFoundException e) {
-      System.out.println(e.getMessage());
-    }
+	    // Look for edge operators -- (or ->) and determine the left and right
+	    // vertices for each edge.
+	    while (s.hasNext()) {
+	    	String[] substring = line.split(edgeOp);
 
-    // Determine if graph is directed or not (i.e., look for "digraph id {" or
-    // "graph id {")
-    String line = "", edgeOp = "";
-    while (s.hasNext()) {
-      line = s.next();
+	    	for (int i = 0; i < substring.length - 1; i += 2) {
+	    		// remove " and trim whitespace from node string on the left
+	    		String vertex1 = substring[0].replace("\"", "").trim();
+	    		// if string is empty, try again
+	    		if (vertex1.equals(""))
+	    			continue;
 
-      // Skip //-style comments.
-      line = line.replaceFirst("//.*", "");
+	    		// do the same for the node string on the right
+	    		String vertex2 = substring[1].replace("\"", "").trim();
+	    		if (vertex2.equals(""))
+	    			continue;
 
-      if (line.indexOf("digraph") >= 0) {
-        g.setDirected(true); // Denotes that graph is directed (CHANGE AS NEEDED)
-        edgeOp = "->";
-        line = line.replaceFirst(".*\\{", "");
-        break;
-      }
-      if (line.indexOf("graph") >= 0) {
-        g.setDirected(false); // Denotes that graph is undirected (CHANGE AS NEEDED)
-        edgeOp = "--";
-        line = line.replaceFirst(".*\\{", "");
-        break;
-      }
-    }
+	    		// add edge between vertex1 and vertex2 (CHANGE AS NEEDED)
+	    		g.addEdge(vertex1, vertex2);
+	    	}
 
-    // Look for edge operators -- (or ->) and determine the left and right
-    // vertices for each edge.
-    while (s.hasNext()) {
-      String[] substring = line.split(edgeOp);
+	    	// do until the "}" has been read
+	    	if (substring[substring.length - 1].indexOf("}") >= 0)
+	    		break;
 
-      for (int i = 0; i < substring.length - 1; i += 2) {
-        // remove " and trim whitespace from node string on the left
-        String vertex1 = substring[0].replace("\"", "").trim();
-        // if string is empty, try again
-        if (vertex1.equals(""))
-          continue;
+	    	line = s.next();
 
-        // do the same for the node string on the right
-        String vertex2 = substring[1].replace("\"", "").trim();
-        if (vertex2.equals(""))
-          continue;
-
-        // add edge between vertex1 and vertex2 (CHANGE AS NEEDED)
-        g.addEdge(vertex1, vertex2);
-      }
-
-      // do until the "}" has been read
-      if (substring[substring.length - 1].indexOf("}") >= 0)
-        break;
-
-      line = s.next();
-
-      // Skip //-style comments.
-      line = line.replaceFirst("//.*", "");
-    }
-    s.close();
-    return g;
-  }
-  
+	    	// Skip //-style comments.
+	    	line = line.replaceFirst("//.*", "");
+	    }
+	    return g;
+	}
 }
